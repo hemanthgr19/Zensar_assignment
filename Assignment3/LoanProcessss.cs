1347ce4f232c339d7f2bc10ee7d443ba892953cf
hemanthgr19 <59571812+hemanthgr19@users.noreply.github.com>
2022-01-28 18:37:57 +0530

    Add files via upload

---
+++ Assignment3/LoanProcessss.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookTrainTicket
+{
+    class BalanceException : ApplicationException
+    {
+        public BalanceException(string msg) : base(msg)
+        {
+
+        }
+    }
+    class LoanProcessss
+    {
+        public int Loan_no;
+        public string Customer_Name;
+        public double Loan_amount;
+        public double Emi_amount;
+        public double Account_balance;
+        public double r;
+        public int y;
+
+        public LoanProcessss(long loanNo, string name)
+        {
+            Loan_no = (int)loanNo;
+            Customer_Name = name;
+        }
+        public double calculate_EMI()
+        {
+            r = 13/100;
+            y = 36;
+            Loan_amount = 500000;
+            
+            Emi_amount = (Loan_amount * Math.Pow((r / 12) + 1, (y)) * r / 12) / (Math.Pow((r / 12) + 1, (y)) - 1);
+            return (Emi_amount);
+
+        }
+
+        public void check_balance()
+        {
+            Console.Write("Enter Account Balance:  ");
+            Account_balance = Convert.ToDouble(Console.ReadLine());
+            if(Account_balance < Emi_amount)
+            {
+                throw (new BalanceException("You dont have sufficient balance in your to clear your EMI amount"));
+            }
+            else
+            {
+                Console.WriteLine("EMI amount id debeted from your account");
+            }
+        }
+
+        
+    }
+
+    class Loan
+    {
+        public void Main(string[] args)
+        {
+            LoanProcessss Lp = new LoanProcessss(123456789, "Hemanth");
+            Lp.calculate_EMI();
+            try
+            {
+                Lp.check_balance();
+
+            }
+            catch (BalanceException be)
+            {
+                Console.WriteLine(be.Message);
+            }
+        }            
+
+
+
+
+    }
+}
---
+++ Assignment3/Tickets.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace BookTrainTicket
+{
+     class TicketBooking : ApplicationException
+     {
+        public TicketBooking(string msg) : base(msg)
+        {
+
+        }
+
+
+    }
+
+     class Passenger
+     {
+        public string Name;
+        public int age;
+
+        public void GetData()
+        {
+            Console.Write("Enter Passenger Name: ");
+            Name = Console.ReadLine();
+            Console.Write("Enter Passenger Age:");
+            age = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void TicketBooking(int NO_of_Ticket)
+        {
+            
+            if (NO_of_Ticket < 5)
+            {
+                
+                throw (new TicketBooking("Ticket Booking succesfully"));
+            }
+            else
+                Console.WriteLine("Cannot Book More then 2 Tickets");
+        }
+
+
+
+
+     }
+     class Test
+     {
+        static void Main(string[] args)
+        {
+            Passenger P = new Passenger();
+            //P.Name = Console.ReadLine();    
+            //P.age = Convert.ToInt32(Console.ReadLine());    
+            P.GetData();
+            Console.Write("Enter no of Tickets you Want to Book: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                P.TicketBooking(n);
+            }
+            catch (TicketBooking Pe)
+            {
+                Console.WriteLine(Pe.Message + " " + Pe.Source);
+            }
+            
+        }
+     }
+
+
+
+}
