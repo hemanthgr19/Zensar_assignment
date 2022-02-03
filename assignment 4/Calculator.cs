02024ff91890534200c1077e386c3f8adbd4976c
hemanthgr19 <59571812+hemanthgr19@users.noreply.github.com>
2022-02-03 08:39:21 +0530

    Add files via upload

---
+++ assignment 4/Calculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CalculateConcession
+{
+    public class Concession
+    {
+        int TotalFare = 500;
+        int Age;
+
+        public void CalculateConcession1()
+        {
+            Console.WriteLine("Enter Age of Citizen : ");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (Age <= 5)
+            {
+                Console.WriteLine("Little Champs-Free Ticket");
+            }
+            else if (Age > 60)
+            {
+                double CalculatedFare = TotalFare * 0.03;
+                double Fare = TotalFare - CalculatedFare;
+                Console.WriteLine("Senior Citizen has to pay:Rs.{0}", Fare);
+            }
+            else
+            {
+                Console.WriteLine("Ticked Booked with a payment of Rs.{0}", TotalFare);
+            }
+        }
+
+
+
+    }
+}
+
+
+
+
+
